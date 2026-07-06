efd3a20393e147ed7fac62b16c746c5bb811a728
Ruddy <ruddy391@gmail.com>
2026-07-06 19:56:48 +0200

    feat: normalize location entities

---
+++ sql/normalization.sql
@@ -0,0 +1,19 @@
+CREATE TABLE continents (
+    continent_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    continent_name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE countries (
+    country_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    country_name TEXT NOT NULL,
+    country_code TEXT NOT NULL UNIQUE,
+    continent_id INTEGER NOT NULL,
+    FOREIGN KEY (continent_id) REFERENCES continents(continent_id)
+);
+
+CREATE TABLE cities (
+    city_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    city_name TEXT NOT NULL,
+    country_id INTEGER NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(country_id)
+);
